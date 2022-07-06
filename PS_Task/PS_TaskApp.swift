83b481abdf3e15d26cbb3f90eb5714d89526d202
Esraa Lotfy <83614911+esraa-lotfy5@users.noreply.github.com>
2022-07-06 12:31:50 -0700

    Initial Commit

---
+++ PS_Task/PS_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PS_TaskApp.swift
+//  PS_Task
+//
+//  Created by Esraa Lotfy  on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PS_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
